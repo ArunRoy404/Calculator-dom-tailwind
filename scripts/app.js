3c689d48adeda1b5f17bfca7d8407e8d24315d9f
ArunRoy404 <arunroy0065@gmail.com>
2025-03-08 13:29:39 +0600

    final push

--- scripts/app.js
+++ scripts/app.js
@@ -2,13 +2,22 @@ let result = 0
 let lastOp = ''
 let op = ''
 let value = 0
+let resultShown = false
 
 document.getElementById('button-container').addEventListener('click', (e) => {
     const btnText = e.target.innerText;
     if (btnText >= 0 && btnText <= 9) {
+        if(resultShown){
+            clearDisplay()
+            resultShown = false
+        }
         addDisplayValue(btnText)
     }
     else if (btnText === '.' && getElement('dot').value === 'false') {
+        if(resultShown){
+            clearDisplay()
+            resultShown = false
+        }
         addDisplayValue(btnText)
         getElement('dot').value = 'true'
     }
@@ -25,8 +34,9 @@ document.getElementById('button-container').addEventListener('click', (e) => {
     else if (btnText === '=') {
         value = getDisplayValue()
         result = operation(result, value, lastOp)
-        setDisplay(result)
+        setDisplay(result.toFixed(2))
         lastOp = ''
+        resultShown = true
     }
     else {
         value = getDisplayValue()
@@ -38,6 +48,5 @@ document.getElementById('button-container').addEventListener('click', (e) => {
         }
         lastOp = op
         clearDisplay()
-        console.log(result)
     }
 })
